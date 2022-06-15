d0fe0bf9aa7e216c3fd739422f9c0779e6880182
Kendokai <kendi.campos@hotmail.com>
2022-06-15 16:48:03 -0300

    Push

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const timer = document.querySelector("timer");
+      init = document.querySelector("init");
+      pause = document.querySelector("pause");
+      reset = document.querySelector("reset");
+
+init.addEventListener("click", () => {
+    alert("Hello World!"); 
+});
