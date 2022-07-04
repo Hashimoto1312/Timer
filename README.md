1cbe4979d5d491f067942383b7b7498c896b68c9
Kendokai <kendi.campos@hotmail.com>
2022-07-04 15:00:52 -0300

    Readme e imagens

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Timer
+# ⏰ Timer
+
+<h3 align="center"> 🚀 Timer ☀️ Concluído ✅ </h3>
+
+<p align="center">
+   <a href=#sobre>Sobre</a> •
+   <a href=#site>Site</a> •
+   <a href=#clone>Clone</a> •
+   <a href=#layout>Layout</a> •
+   <a href=#tecnologias>Tecnologias</a> • 
+   <a href=#licença>Licença</a> •
+   <a href=#autor>Autor</a> 
+</p>
+
+## 💻 Sobre
+📱 Um timer simples que inicia o tempo, pausa e reseta
+
+---
+
+## 🌐 Site
+<a href="https://vermillion-empanada-8efb96.netlify.app">📎 Clique aqui</a> 
+
+---
+
+## 📥 Clone
+```bash
+git clone https://github.com/Hashimoto1312/Timer.git
+```
+
+---
+
+## 🎨 Layout
+![Layout!](assets/img/layout.png "layout")
+
+---
+
+## 👾 Tecnologias
+<img align="center" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg">
+<img align="center" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg">
+<img align="center" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+
+---
+
+## 👨‍💻 Autor
+ <img style="border-radius: 50%;" src="https://avatars.githubusercontent.com/u/71889483?v=4" width="100px;" alt=""/>
+ <br/>🚀 Kendi Hashimoto
+
+[![Linktree Badge](https://img.shields.io/badge/linktree-1de9b6?style=for-the-badge&logo=linktree&logoColor=white)](https://linktr.ee/Hashimoto01)
+[![Linkedin Badge](https://img.shields.io/badge/-KendiHashimoto-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/tgmarinho/)](https://www.linkedin.com/in/kendi-hashimoto-202359220/) 
+[![Gmail Badge](https://img.shields.io/badge/-kendcampos@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:tgmarinho@gmail.com)](https://mail.google.com/mail/u/0/#inbox)
+
+---
+
+## 📝 Licença
+Este projeto esta sobe a licença [MIT](./LICENSE).
--- src/script.js
+++ src/script.js
@@ -1,45 +1,44 @@
 watch = () => {
+   
+   // FORMATA SEGUNDOS EM FORMATO DE HORARIO
+   getTimeFromSeconds = (seconds) => {
+       const data = new Date(seconds * 1000);
+       return data.toLocaleTimeString("pt-BR", {
+           hour12: false,
+           timeZone: "UTC"
+       });
+   }
 
-    // FORMATA SEGUNDOS EM FORMATO DE HORARIO
-    getTimeFromSeconds = (seconds) => {
-        const data = new Date(seconds * 1000);
-        return data.toLocaleTimeString("pt-BR", {
-            hour12: false,
-            timeZone: "UTC"
-        });
-    }
+   const timer = document.getElementById("timer");
+   let seconds = 0;
+   let watch;
 
-    const timer = document.getElementById("timer");
-    let seconds = 0;
-    let watch;
+   initTimer = () => {
+      watch = setInterval(() => {
+          seconds++;
+          timer.innerHTML = getTimeFromSeconds(seconds);
+      }, 1000)
+  }
+  document.addEventListener("click", (e) => {
+      const el = e.target;
 
-    initTimer = () => {
-        watch = setInterval(() => {
-            seconds++;
-            timer.innerHTML = getTimeFromSeconds(seconds);
-        }, 1000)
-    }
+      if (el.classList.contains("init")) {
+          timer.classList.remove("paused");
+          clearInterval(watch);
+          initTimer(); 
+      }
 
-    document.addEventListener("click", (e) => {
-        const el = e.target;
+      if (el.classList.contains("pause")) {
+          clearInterval(watch);
+          timer.classList.add("paused");
+      }
 
-        if (el.classList.contains("init")) {
-            timer.classList.remove("paused");
-            clearInterval(watch);
-            initTimer(); 
-        }
-
-        if (el.classList.contains("pause")) {
-            clearInterval(watch);
-            timer.classList.add("paused");
-        }
-
-        if (el.classList.contains("reset")) {
-            timer.classList.remove("paused");
-            clearInterval(watch);
-            timer.innerHTML = "00:00:00";
-            seconds = 0;
-        }
-    })
+      if (el.classList.contains("reset")) {
+          timer.classList.remove("paused");
+          clearInterval(watch);
+          timer.innerHTML = "00:00:00";
+          seconds = 0;
+      }
+  })
 }
 watch();
